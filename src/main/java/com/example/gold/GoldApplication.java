454c49b174b61e489586cd40a9691b4f01a3e421
Jai singh <bhatijaisingh678@gmail.com>
2022-07-04 13:43:54 +0530

    new changes

---
+++ src/main/java/com/example/gold/GoldApplication.java
@@ -0,0 +1,14 @@
+package com.example.gold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldApplication.class, args);
+		System.out.println("Hiiiiii");
+	}
+
+}
